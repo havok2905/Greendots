346efaadcfdd937884a990a0f3ee78054570af9b
mclean.cw <havok2905@gmail.com>
2013-11-07 19:55:57 -0500

    added step definitions and ran tests

--- features/beers.feature
+++ features/beers.feature
@@ -5,19 +5,19 @@ Feature: Manage Beers
 
 @start
 Scenario: Start on the beer home page
-  Given I have no beer
-  And I go to the beer page
-  Then I should see no beer on the beer home page
-
-Scenario: Create and see view a new beer
   Given I go to the beer page
-  And I go to the edit beer page for 1
+  Then I should see no beer information
+
+@create
+Scenario: Create and see view a new beer
+  Given I go to the create beer page
   And I fill out the beer form with beer data
   And I submit a beer
   And I go to the view beer page for 1
-  Then I should see a beer summary
+  Then I should see beer information
 
+@check
 Scenario: Go back to the beer home page
   Given I have a beer
   And I go to the beer page
-  Then I should see beer on the beer home page
+  Then I should see beer information
--- features/step_definitions/beers_steps.rb
+++ features/step_definitions/beers_steps.rb
@@ -1,32 +1,64 @@
 
-# Steps for starting with or without beer models
-Given(/^I have a beer$/) do
+# Steps for beer models
+Given /^I have a beer$/ do
+  beer = Beer.new
+  beer.name = "Maduro"
+  beer.brewery = "Cigar City"
+  beer.year = "2013"
+  beer.style = "Brown Ale"
+  beer.rating = "5"
+  beer.save
 end
-Given(/^I have no beer$/) do
-end
-Given(/^A beer exists$/) do
-end
+
+
 
 # Form Actions
-Given(/^I fill out the beer form with beer data$/) do
+Given /^I fill out the beer form with beer data$/ do
+  fill_in "beer_name", :with => "Maduro"
+  fill_in "beer_brewery", :with => "Cigar City"
+  fill_in "beer_year", :with => "2013"
+  fill_in "beer_style", :with => "Brown Ale"
+  fill_in "beer_rating", :with => "5"
 end
+
+
 
 # Seeing beer data
-Then(/^I should see beer on the beer home page$/) do
-end
-Then(/^I should see no beer on the beer home page$/) do
-end
-Then(/^I should see a beer summary$/) do 
+Then /^I should see beer information$/ do
+  page.should have_content "Maduro"
+  page.should have_content "Cigar City"
+  page.should have_content "2013"
+  page.should have_content "Brown Ale"
+  page.should have_content "5"
 end
 
+Then /^I should see no beer information$/ do
+  page.should have_no_content "Maduro"
+  page.should have_no_content "Cigar City"
+  page.should have_no_content "2013"
+  page.should have_no_content "Brown Ale"
+  page.should have_no_content "5"
+end
+
+
+
 # Page navigation
-When(/^I click on new beer$/) do
+When /^I click on new beer$/ do
+  click_link 'New Beer'
 end
-When(/^I submit a beer$/) do
+
+When /^I submit a beer$/ do
+  click_button 'Create Beer'
 end
-When(/^I go to the beer page$/) do
+
+When /^I go to the beer page$/ do
+  visit '/beers/'
 end
-When(/^I go to the edit beer page for (\d+)$/) do |id|
+
+When /^I go to the create beer page$/ do
+  visit '/beers/new/'
 end
-When(/^I go to the view beer page for (\d+)$/) do |id|
+
+When /^I go to the view beer page for (\d+)$/ do |id|
+  visit '/beers/' + id
 end
